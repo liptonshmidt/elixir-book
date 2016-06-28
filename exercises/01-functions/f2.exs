5e540ef302fd8dea7fdea4c95a35516114f1d013
Alexey Lipnyagov <liptonshmidt@gmail.com>
2016-06-28 13:31:08 +0300

    Reading chapters 1-5

---
+++ exercises/01-functions/f2.exs
@@ -0,0 +1,6 @@
+# Write a function that takes three arguments.
+# If the first two are zero, return “FizzBuzz.”
+# If the first is zero, return “Fizz.”
+# If the second is zero, return “Buzz.”
+# Otherwise return the third argument.
+# Do not use any language features that we haven’t yet covered in this book.
