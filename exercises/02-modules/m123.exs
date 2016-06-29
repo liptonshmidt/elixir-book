eaeb21c09bff665b67bd191151e1ca2dc1ea460b
Alexey Lipnyagov <liptonshmidt@gmail.com>
2016-06-29 13:05:11 +0300

    Modules-1,2,3

--- exercises/02-modules/m123.exs
+++ exercises/02-modules/m123.exs
@@ -1,7 +1,8 @@
 # Extend the Times module with a triple function that multiplies its parameter by three.
+# Add a quadruple function. (Maybe it could call the double function....)
 
 defmodule Times do
-  def triple(n) do
-    n * 3
-  end
+  def double(n), do: n*2
+  def triple(n), do: n * 3
+  def quadraple(n), do: double double n
 end
