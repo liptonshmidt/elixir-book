21f736ef8a020bd8ba86514c03bfcbf18932b9b6
Alexey Lipnyagov <liptonshmidt@gmail.com>
2016-11-25 15:57:43 +0300

    Implementing Enum exercises

--- exercises/05-lists/l5.exs
+++ exercises/05-lists/l5.exs
@@ -3,12 +3,73 @@
 # all?, each, filter, split, and take.
 
 defmodule MyList do
-  def all?([], f), do: true
+  def all?(list), do: all?(list, fn x -> !!x end)
+  def all?([], _f), do: true
   def all?([ h | t ], f) do
     f.(h) && all?(t, f)
   end
+
+  def each([], _f), do: []
+  def each([h | t], f) do
+    [ f.(h) | each(t, f) ]
+  end
+
+  def filter([], _f), do: []
+  def filter([h | t], f) do
+    if f.(h) do
+      [h | filter(t, f)]
+    else
+      filter(t, f)
+    end
+  end
+
+  # From Enum docs:
+  # split(enumerable, count)
+  # Splits the enumerable into two enumerables, leaving count elements in the first one. 
+  # If count is a negative number, it starts counting from the back to the beginning of the enumerable
+
 end
 
+
+# all? specs
+IO.inspect MyList.all?([])
+IO.inspect MyList.all?([1, 2, 3])
+IO.inspect MyList.all?([true, true])
+IO.inspect MyList.all?([true, false])
 IO.inspect MyList.all?([], &(&1 > 0))
 IO.inspect MyList.all?([-1,0,1], &(&1 > 0))
 IO.inspect MyList.all?([1,2,3], &(&1 > 0))
+
+
+
+
+
+# each specs
+MyList.each([], &(IO.puts &1))
+MyList.each([1, 2, 3], &(IO.puts &1))
+
+
+
+
+
+# filter specs
+IO.inspect MyList.filter([1,2,3,4], &(&1 > 2))
+
+
+
+
+# split specs
+IO.inspect MyList.split([1, 2, 3, 4, 5, 6], 3)
+# => [1, 2, 3], [4, 5, 6]
+
+IO.inspect MyList.split([1, 2, 3], 0)
+# => [], [1, 2, 3]
+
+IO.inspect MyList.split([1, 2, 3], 3)
+# => [1, 2, 3], []
+
+IO.inspect MyList.split([1, 2, 3], 10)
+# => [1, 2, 3], []
+
+IO.inspect MyList.split([1, 2, 3], -1)
+# => [3], [2, 1]
