dc143063a8e3cf5630e5401423416cb7f7fe11cf
Alexey Lipnyagov <liptonshmidt@gmail.com>
2016-12-01 18:46:20 +0300

    Strings: ex.6 (wip)

---
+++ exercises/07-strings-and-binaries/ex6.exs
@@ -0,0 +1,7 @@
+# Write a function to capitalize the sentences in a string.
+# Each sentence is terminated by a period and a space.
+# Right now, the case of the characters in the string is random.
+
+
+IO.inspect MyString.capitalize_sequances("oh. a DOG. woof. ")
+# =>  "Oh. A dog. Woof"
