029db07e8fb0e7a7b2f105f6aacccb3465aa355e
Alexey Lipnyagov <liptonshmidt@gmail.com>
2016-12-02 00:22:18 +0300

    Strings: ex.6

--- exercises/07-strings-and-binaries/ex6.exs
+++ exercises/07-strings-and-binaries/ex6.exs
@@ -2,6 +2,15 @@
 # Each sentence is terminated by a period and a space.
 # Right now, the case of the characters in the string is random.
 
+import Enum, only: [map: 2, join: 2]
+import String, only: [split: 2, capitalize: 1]
+
+defmodule MyString do
+  def capitalize_sequances(str) do
+    split(str, ". ") |> map(&capitalize/1) |> join(". ")
+  end
+end
+
 
 IO.inspect MyString.capitalize_sequances("oh. a DOG. woof. ")
 # =>  "Oh. A dog. Woof"
