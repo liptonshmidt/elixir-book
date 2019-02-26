f59ff3c5cf9045395a162e468b9dbefa96c36e16
Alexey Lipnyagov <liptonshmidt@gmail.com>
2019-02-26 13:17:59 +0300

    GenServer - simple only pop stack

--- 1.6_plus/17.OTP:Servers/stack/lib/stack/server.ex
+++ 1.6_plus/17.OTP:Servers/stack/lib/stack/server.ex
@@ -1,3 +1,12 @@
 defmodule Stack.Server do
+  use GenServer
 
+  def init(initial_contents) do
+    {:ok, initial_contents}
+  end
+
+  def handle_call(:pop, _from, contents) do
+    [h|t] = contents
+    {:reply, h, t}
+  end
 end
